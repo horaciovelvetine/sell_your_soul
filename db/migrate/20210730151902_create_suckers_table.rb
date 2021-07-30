7025d7eae9c70fa75ff824db4e28cce96025b1f4
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-07-30 13:39:41 -0600

    DB updates to reflect controller changes

--- db/migrate/20210730151902_create_suckers_table.rb
+++ db/migrate/20210730151902_create_suckers_table.rb
@@ -1,6 +1,7 @@
 class CreateSuckersTable < ActiveRecord::Migration[6.1]
   def change
     create_table :suckers do |t|
+      ##NAMES
       t.string "first_name"
       t.string "middle_name"
       t.string "last_name"
@@ -8,26 +9,46 @@ class CreateSuckersTable < ActiveRecord::Migration[6.1]
       t.string "pseudonym"
       t.string "alias"
       t.string "username"
+      
+      ##PASS
       t.string "password_digest"
+      
+      #PHONES
       t.string "cell_phone_number"
       t.string "home_phone_number"
+      
+      #ADDRESS
       t.string "address_1"
       t.string "address_2"
       t.string "PO_Box"
+      
+      #EMAILS
       t.string "primary_email"
       t.string "secondary_email"
       t.string "spam_email"
+      
+      #TWO PERSONAL
       t.string "yearly_income"
       t.string "social_security_number"
       t.string "primary_bank"
       t.string "credit_score"
+      
+      #BANK RECOVERY 
       t.string "relationship_status"
       t.string "favorite_pet"
       t.string "additional_pet"
       t.string "catch_phrase"
+      
+      #JUST IN CASE THEY FEEL STRONGLY
       t.string "political_affiliation"
       t.string "belief_religion"
       t.string "interests"
+
+      #IN CASE WE MISSED IT
+      t.text "additional_details_1"
+      t.text "additional_details_2"
+      
+      #BEHIND THE SCENES
       t.real "balance"
 
       t.timestamps null: false
---
+++ db/migrate/20210730193020_create_bank_table.rb
@@ -0,0 +1,10 @@
+class CreateTransactionTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table "transactions" do |t|
+      t.real "amount"
+      t.
+    end
+
+    
+  end
+end
