22e9872fb3dd9169409bc4da8d28e2a2a6a9272f
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-07-30 12:51:27 -0600

    add has/belongs relationships

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -1,2 +1,3 @@
 class Corporation < ActiveRecord::Base
+  has_and_belongs_to_many :suckers
 end
--- app/models/sucker.rb
+++ app/models/sucker.rb
@@ -1,2 +1,4 @@
 class Sucker < ActiveRecord::Base
+  has_and_belongs_to_many :corporations
+  has_secure_password
 end
