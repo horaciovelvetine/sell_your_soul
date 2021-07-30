2e4a977c82f22499cfc20d1acee04527fd12a7dd
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-07-30 11:12:26 -0600

    add basic logg in/out function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,18 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get "signup" do
+    
+    erb :signup
+  end
+
+    def logged_in?
+    !!current_sucker
+  end
+
+  def current_sucker
+    @current_sucker ||= Sucker.find_by(id: session[:sucker_id]) if session[:sucker_id]
+  end
+end
+
 end
