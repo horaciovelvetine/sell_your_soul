bc452b56d8d181b48af05f9d19729320bf5d266d
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-08-01 18:58:47 -0600

    seed the databases

---
+++ db/seeds.rb
@@ -0,0 +1,515 @@
+#List of a Shitload of Companies
+corporations_list = {
+"Huntington Bancshares" => {:payout_amount => 0.00067 },
+"Post Holdings" => {:payout_amount => 0.00068 },
+"Cerner" => {:payout_amount => 0.000487 },
+"Simon Property Group" => {:payout_amount => 0.00035 },
+"Crown Castle International" => {:payout_amount => 0.00089 },
+"Levi Strauss" => {:payout_amount => 0.00061 },
+"Franklin Resources" => {:payout_amount => 0.00048 },
+"Rush Enterprises" => {:payout_amount => 0.00091 },
+"Commercial Metals" => {:payout_amount => 0.00053 },
+"Realogy Holdings" => {:payout_amount => 0.00071 },
+"Williams-Sonoma" => {:payout_amount => 0.00048 },
+"Ameren" => {:payout_amount => 0.000487 },
+"Kroger" => {:payout_amount => 0.00098 },
+"Marathon Petroleum" => {:payout_amount => 0.00028 },
+"Microsoft" => {:payout_amount => 0.00067 },
+"Verizon Communications" => {:payout_amount => 0.000398 },
+"Walgreens Boots Alliance" => {:payout_amount => 0.000378 },
+"General Motors" => {:payout_amount => 0.0003 },
+"JPMorgan Chase" => {:payout_amount => 0.0008 },
+"Cardinal Health" => {:payout_amount => 0.0006 },
+"Chevron" => {:payout_amount => 0.00023 },
+"Costco Wholesale" => {:payout_amount => 0.0004 },
+"Cigna" => {:payout_amount => 0.00098 },
+"Ford Motor" => {:payout_amount => 0.00078 },
+"Alphabet" => {:payout_amount => 0.00056 },
+"AmerisourceBergen" => {:payout_amount => 0.00067 },
+"AT&T" => {:payout_amount => 0.0003 },
+"McKesson" => {:payout_amount => 0.000653 },
+"UnitedHealth Group" => {:payout_amount => 0.0003 },
+"Berkshire Hathaway" => {:payout_amount => 0.0004 },
+"CVS Health" => {:payout_amount => 0.001 },
+"Apple" => {:payout_amount => 0.00067 },
+"Exxon Mobil" => {:payout_amount => 0.00078 },
+"Amazon.com" => {:payout_amount => 0.0000042 },
+"Walmart" => {:payout_amount => 0.00081 },
+"Analog Devices" => {:payout_amount => 0.00063 },
+"TD Ameritrade Holding" => {:payout_amount => 0.000409 },
+"Tapestry" => {:payout_amount => 0.00018 },
+"Avantor" => {:payout_amount => 0.00091 },
+"EnLink Midstream" => {:payout_amount => 0.0001 },
+"Robert Half International" => {:payout_amount => 0.0003 },
+"Olin" => {:payout_amount => 0.00218 },
+"TravelCenters of America" => {:payout_amount => 0.00018 },
+"HD Supply Holdings" => {:payout_amount => 0.00087 },
+"NetApp" => {:payout_amount => 0.000673 },
+"Graphic Packaging Holding" => {:payout_amount => 0.000513 },
+"First American Financial" => {:payout_amount => 0.00087 },
+"Ingredion" => {:payout_amount => 0.000123 },
+"Clorox" => {:payout_amount => 0.000245 },
+"Ascena Retail Group" => {:payout_amount => 0.000145 },
+"R.R. Donnelley & Sons" => {:payout_amount => 0.000218 },
+"Celanese" => {:payout_amount => 0.000278 },
+"Ralph Lauren" => {:payout_amount => 0.000113 },
+"Dillard’s" => {:payout_amount => 0.000567 },
+"AK Steel Holding" => {:payout_amount => 0.000317 },
+"Fannie Mae" => {:payout_amount => 0.000457 },
+"Science Applications International" => {:payout_amount => 0.000318 },
+"Apache" => {:payout_amount => 0.000213 },
+"GameStop" => {:payout_amount => 0.000113 },
+"Activision Blizzard" => {:payout_amount => 0.00003 },
+"ABM Industries" => {:payout_amount => 0.000183 },
+"Hess" => {:payout_amount => 0.000123 },
+"American Axle & Manufacturing" => {:payout_amount => 0.000113 },
+"Intercontinental Exchange" => {:payout_amount => 0.00003 },
+"Brighthouse Financial" => {:payout_amount => 0.000413 },
+"Bank of America" => {:payout_amount => 0.000318 },
+"Weyerhaeuser" => {:payout_amount => 0.000508 },
+"Alliance Data Systems" => {:payout_amount => 0.000113 },
+"Securian Financial Group" => {:payout_amount => 0.000403 },
+"Home Depot" => {:payout_amount => 0.000112 },
+"Wynn Resorts" => {:payout_amount => 0.000213 },
+"O-I Glass" => {:payout_amount => 0.000401 },
+"Rockwell Automation" => {:payout_amount => 0.000311 },
+"S&P Global" => {:payout_amount => 0.000983 },
+"Booz Allen Hamilton Holding" => {:payout_amount => 0.000043 },
+"Ovintiv" => {:payout_amount => 0.000223 },
+"Advanced Micro Devices" => {:payout_amount => 0.000999 },
+"FM Global" => {:payout_amount => 0.000783 },
+"Regions Financial" => {:payout_amount => 0.000443 },
+"Intuit" => {:payout_amount => 0.000223 },
+"Seaboard" => {:payout_amount => 0.000103 },
+"CMS Energy" => {:payout_amount => 0.000223 },
+"Polaris" => {:payout_amount => 0.000123 },
+"Cintas" => {:payout_amount => 0.000113 },
+"Northern Trust" => {:payout_amount => 0.000013 },
+"NCR" => {:payout_amount => 0.000709 },
+"M&T Bank" => {:payout_amount => 0.00043 },
+"Phillips 66" => {:payout_amount => 0.000993 },
+"Packaging Corp. of America" => {:payout_amount => 0.000223 },
+"Hanesbrands" => {:payout_amount => 0.000443 },
+"Avery Dennison" => {:payout_amount => 0.000111 },
+"Beacon Roofing Supply" => {:payout_amount => 0.000882 },
+"Dover" => {:payout_amount => 0.00632 },
+"Magellan Health" => {:payout_amount => 0.000773 },
+"Owens Corning" => {:payout_amount => 0.000273 },
+"MasTec" => {:payout_amount => 0.000711 },
+"Arthur J. Gallagher" => {:payout_amount => 0.000743 },
+"Asbury Automotive Group" => {:payout_amount => 0.000173 },
+"Old Republic International" => {:payout_amount => 0.000003 },
+"Toll Brothers" => {:payout_amount => 0.000273 },
+"Builders FirstSource" => {:payout_amount => 0.000993 },
+"Comcast" => {:payout_amount => 0.000213 },
+"Burlington Stores" => {:payout_amount => 0.000763 },
+"Dell Technologies" => {:payout_amount => 0.000103 },
+"Motorola Solutions" => {:payout_amount => 0.000893 },
+"UGI" => {:payout_amount => 0.000223 },
+"Fortive" => {:payout_amount => 0.000983 },
+"Dean Foods" => {:payout_amount => 0.000113 },
+"Blackstone Group" => {:payout_amount => 0.000443 },
+"Devon Energy" => {:payout_amount => 0.000453 },
+"Ulta Beauty" => {:payout_amount => 0.000173 },
+
+
+}
+
+corporations_list.each do |name, corporation_hash|
+  c = Corporation.new
+  c.name = name
+  corporation_hash.each do |attribute, value|
+      c[attribute] = value
+  end
+  c.save
+end
+
+
+# Some more corps in case. 
+# Anthem
+# Wells Fargo
+# Citigroup
+# Valero Energy
+# General Electric
+# Johnson & Johnson
+# State Farm Insurance
+# Target
+# IBM
+# Raytheon Technologies
+# Boeing
+# Freddie Mac
+# Centene
+# United Parcel Service
+# Lowe’s
+# Intel
+# Facebook
+# FedEx
+# MetLife
+# Walt Disney
+# Procter & Gamble
+# PepsiCo
+# Humana
+# Prudential Financial
+# Archer Daniels Midland
+# Albertsons
+# Sysco
+# Lockheed Martin
+# HP
+# Energy Transfer
+# Goldman Sachs Group
+# Morgan Stanley
+# Caterpillar
+# Cisco Systems
+# Pfizer
+# HCA Healthcare
+# AIG
+# American Express
+# Delta Air Lines
+# Merck
+# American Airlines Group
+# Charter Communications
+# Allstate
+# New York Life Insurance
+# Nationwide
+# Best Buy
+# United Airlines Holdings
+# Liberty Mutual Insurance Group
+# Dow
+# Tyson Foods
+# TJX
+# TIAA
+# Oracle
+# General Dynamics
+# Deere
+# Nike
+# Progressive
+# Publix Super Markets
+# Coca-Cola
+# Massachusetts Mutual Life Insurance
+# Tech Data
+# World Fuel Services
+# Honeywell International
+# ConocoPhillips
+# USAA
+# Exelon
+# Northrop Grumman
+# Capital One Financial
+# Plains GP Holdings
+# AbbVie
+# INTL FCStone
+# Enterprise Products Partners
+# Northwestern Mutual
+# 3M
+# Abbott Laboratories
+# CHS
+# Travelers
+# Philip Morris International
+# Raytheon
+# Hewlett Packard Enterprise
+# Arrow Electronics
+# ViacomCBS
+# Dollar General
+# U.S. Bancorp
+# Starbucks
+# Bristol-Myers Squibb
+# US Foods Holding
+# Mondelez International
+# Paccar
+# Thermo Fisher Scientific
+# Macy’s
+# Jabil
+# Kraft Heinz
+# Duke Energy
+# Tesla
+# PBF Energy
+# Qualcomm
+# NGL Energy Partners
+# CBRE Group
+# Baker Hughes
+# Synnex
+# Dollar Tree
+# Cummins
+# United Natural Foods
+# Micron Technology
+# Amgen
+# Penske Automotive Group
+# Visa
+# Broadcom
+# Nucor
+# Gilead Sciences
+# Southwest Airlines
+# Halliburton
+# CenturyLink
+# International Paper
+# Eli Lilly
+# Aflac
+# Lennar
+# Occidental Petroleum
+# Union Pacific
+# Rite Aid
+# PNC Financial Services Group
+# DuPont
+# Southern
+# AutoNation
+# DXC Technology
+# McDonald’s
+# Marriott International
+# ManpowerGroup
+# Bank of New York Mellon
+# Hartford Financial Services Group
+# Danaher
+# Whirlpool
+# AECOM
+# Netflix
+# Kohl’s
+# Lear
+# Altria Group
+# Performance Food Group
+# Avnet
+# Synchrony Financial
+# Genuine Parts
+# NextEra Energy
+# CarMax
+# Tenet Healthcare
+# Kimberly-Clark
+# Emerson Electric
+# WestRock
+# CDW
+# Jones Lang LaSalle
+# Sherwin-Williams
+# Fluor
+# PayPal Holdings
+# D.R. Horton
+# HollyFrontier
+# Tenneco
+# EOG Resources
+# Becton Dickinson
+# Lincoln National
+# PG&E
+# salesforce.com
+# Mastercard
+# General Mills
+# Molina Healthcare
+# Cognizant Technology Solutions
+# Marsh & McLennan
+# XPO Logistics
+# Dominion Energy
+# Western Digital
+# Gap
+# Aramark
+# Principal Financial
+# Ross Stores
+# Colgate-Palmolive
+# American Electric Power
+# Nordstrom
+# Jacobs Engineering Group
+# Waste Management
+# C.H. Robinson Worldwide
+# PPG Industries
+# Booking Holdings
+# Omnicom Group
+# Loews
+# Ecolab
+# Stryker
+# Estée Lauder
+# Goodyear Tire & Rubber
+# Truist Financial
+# Applied Materials
+# BlackRock
+# Stanley Black & Decker
+# Freeport-McMoRan
+# Texas Instruments
+# Biogen
+# Parker-Hannifin
+# Reinsurance Group of America
+# Howmet Aerospace
+# Automatic Data Processing
+# Uber Technologies
+# Illinois Tool Works
+# DaVita
+# Discover Financial Services
+# Land O’Lakes
+# VF
+# Corteva
+# Las Vegas Sands
+# Textron
+# Kellogg
+# Guardian Life Ins. Co. of America
+# Qurate Retail
+# Core-Mark Holding
+# Community Health Systems
+# Kinder Morgan
+# BJ’s Wholesale Club
+# State Street
+# Ameriprise Financial
+# Global Partners
+# United States Steel
+# L Brands
+# MGM Resorts International
+# L3Harris Technologies
+# DISH Network
+# Lithia Motors
+# DTE Energy
+# American Family Insurance Group
+# Farmers Insurance Exchange
+# Consolidated Edison
+# LKQ
+# Sempra Energy
+# Edison International
+# CenterPoint Energy
+# Quanta Services
+# Murphy USA
+# Expedia Group
+# Group 1 Automotive
+# Bed Bath & Beyond
+# Unum Group
+# CSX
+# AutoZone
+# Pacific Life
+# Vistra Energy
+# Charles Schwab
+# Crown Holdings
+# Ally Financial
+# Laboratory Corp. of America
+# Live Nation Entertainment
+# Xcel Energy
+# Corning
+# W.W. Grainger
+# Ball
+# Fox
+# Universal Health Services
+# Baxter International
+# Norfolk Southern
+# Navistar International
+# Adobe
+# J.C. Penney
+# Discovery
+# Keurig Dr Pepper
+# Leidos Holdings
+# IQVIA Holdings
+# Reliance Steel & Aluminum
+# Nvidia
+# Entergy
+# FirstEnergy
+# eBay
+# Boston Scientific
+# Office Depot
+# Molson Coors Beverage
+# Steel Dynamics
+# Mutual of Omaha Insurance
+# Sonic Automotive
+# Alcoa
+# Fidelity National Information Services
+# Henry Schein
+# Republic Services
+# Liberty Media
+# Peter [hotlink ignore=true]Kiewit[/hotlink] Sons’
+# Interpublic Group
+# PulteGroup
+# AES
+# Fiserv
+# BorgWarner
+# Oneok
+# O’Reilly Automotive
+# Assurant
+# Newell Brands
+# Public Service Enterprise Group
+# News Corp.
+# Calpine
+# Auto-Owners Insurance
+# Mohawk Industries
+# PVH
+# Campbell Soup
+# NRG Energy
+# Fifth Third Bancorp
+# Hertz Global Holdings
+# Altice USA
+# Newmont
+# Cheniere Energy
+# Advance Auto Parts
+# Lam Research
+# Owens & Minor
+# Equitable Holdings
+# Conagra Brands
+# Markel
+# Jones Financial (Edward Jones)
+# Hormel Foods
+# Hilton Worldwide Holdings
+# Univar Solutions
+# United Rentals
+# Pioneer Natural Resources
+# Delek US Holdings
+# Eastman Chemical
+# EMCOR Group
+# Avis Budget Group
+# J.B. Hunt Transport Services
+# Xerox Holdings
+# Wayfair
+# KKR
+# AGCO
+# Alleghany
+# Icahn Enterprises
+# Voya Financial
+# Ryder System
+# Air Products & Chemicals
+# Mosaic
+# Huntington Ingalls Industries
+# Berry Global Group
+# Anixter International
+# Alaska Air Group
+# Yum China Holdings
+# Dick’s Sporting Goods
+# Caesars Entertainment
+# Genworth Financial
+# Targa Resources
+# Coty
+# Dana
+# Thrivent Financial for Lutherans
+# Autoliv
+# SpartanNash
+# Eversource Energy
+# Darden Restaurants
+# Chesapeake Energy
+# National Oilwell Varco
+# Fidelity National Financial
+# Erie Insurance Group
+# Oshkosh
+# Casey’s General Stores
+# WESCO International
+# Tractor Supply
+# CommScope Holding
+# Huntsman
+# American Financial Group
+# Masco
+# Sanmina
+# Amphenol
+# Williams
+# Westinghouse Air Brake
+# Expeditors Intl. of Washington
+# Andersons
+# Westlake Chemical
+# Constellation Brands
+# Frontier Communications
+# JetBlue Airways
+# Citizens Financial Group
+# Raymond James Financial
+# Foot Locker
+# Hershey
+# Zimmer Biomet Holdings
+# Cincinnati Financial
+# Western & Southern Financial Group
+# W.R. Berkley
+# Thor Industries
+# Regeneron Pharmaceuticals
+# Spirit AeroSystems Holdings
+# J.M. Smucker
+# PPL
+# Insight Enterprises
+# Quest Diagnostics
+# KeyCorp
+# Veritiv
+# DCP Midstream
+# American Tower
+# Graybar Electric
+# WEC Energy Group
+# NVR
