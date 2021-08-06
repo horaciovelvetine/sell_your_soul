2fd475c02677389bcd32f62173e7460957bb64bd
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-08-06 11:51:49 -0600

    remove surplus password validation

--- app/models/sucker.rb
+++ app/models/sucker.rb
@@ -6,7 +6,6 @@ class Sucker < ActiveRecord::Base
   has_many :corporations, through: :sucker_corps
 
   validates  :username, presence: true, uniqueness: true
-  validates  :password, presence: true
   validates_presence_of :first_name, :last_name, :primary_email
   
 end
